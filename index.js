d524164a30d8abb5eb27754547af166a2d0141bc
jeremybgriffiths <gri18025@byui.edu>
2020-07-06 22:20:06 -0600

    Fixing UI elements

--- index.js
+++ index.js
@@ -17,9 +17,9 @@ const pool = new Pool({
 });
 
 const getRecipes = (req, res) => {
-  const ingredient = req.query.ingredient;
+  const ingredients = req.query.ingredients;
 
-  const url = `${process.env.API_URL}${process.env.API_ID}${process.env.API_KEY}&q=${ingredient}`;
+  const url = `${process.env.API_URL}${process.env.API_ID}${process.env.API_KEY}&to=20&q=${ingredients}`;
   axios.get(url)
     .then(response => {
       res.status(200).json(response.data)
