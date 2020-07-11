6ae5cc3b9bd99b1e9eb9230884afffdcc45458b7
jeremybgriffiths <gri18025@byui.edu>
2020-07-11 17:54:48 -0600

    Added some other pages and login function

--- index.js
+++ index.js
@@ -1,10 +1,34 @@
+const path = require('path');
 const express = require('express');
+const app = express();
+
+const session = require('express-session');
+
+app.use(session({
+  secret: 'my-super-secret-secret!',
+  resave: false,
+  saveUninitialized: true
+}));
+
 const axios = require('axios');
 
-const app = express();
-const path = require('path');
 require('dotenv').config();
-const PORT = process.env.PORT || 5000
+
+app.use(express.json());
+app.use(express.urlencoded({
+  extended: true
+}));
+
+app.set('port', (process.env.PORT || 5000));
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.listen(app.get('port'), () => {
+  console.log('Node app is running on port', app.get('port'));
+});
+
 const {
   Pool
 } = require("pg");
@@ -29,9 +53,60 @@ const getRecipes = (req, res) => {
     });
 }
 
-app.use(express.static(path.join(__dirname, 'public')));
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+const handleLogin = (req, res) => {
+  let username = req.body.username;
+  let password = req.body.username;
+  let result = {
+    success: false
+  };
+
+  if (username === 'user1' && password === 'user1') {
+    req.session.user = username;
+    result = {
+      success: true
+    };
+  }
+
+  console.log(req.session.user);
+  res.json(result);
+}
+
+const handleLogout = (req, res) => {
+  let result = {
+    success: false
+  };
+
+  if (req.session.user) {
+    req.session.destroy();
+    result = {
+      success: true
+    };
+  }
+
+  res.json(result);
+}
+
+const verifyLogin = (req, res, next) => {
+  if (req.session.user) {
+		next();
+	} else {
+		var result = {success:false, message: "Access Denied"};
+		response.status(401).json(result);
+	}
+}
+
+const logRequest = (req, res, next) => {
+  console.log("Received a request for: " + req.url);
+  next();
+}
+
+app.use(logRequest);
+
 app.get('/', (req, res) => res.render('pages/index.ejs'));
-app.get('/getRecipes', getRecipes);
-app.listen(PORT, () => console.log(`Listening on ${ PORT }`));
+app.get('/home', (req, res) => res.render('pages/home.ejs'));
+app.get('/recipes', (req, res) => res.render('pages/recipes.ejs'));
+
+app.post('/login', handleLogin);
+app.post('/logout', handleLogout);
+
+app.get('/searchRecipes', getRecipes);
---
+++ public/scripts/test.js
@@ -0,0 +1,28 @@
+function login() {
+	var username = $("#username").val();
+	var password = $("#password").val();
+
+	var params = {
+		username: username,
+		password: password
+	};
+
+	$.post("/login", params, function(result) {
+		if (result && result.success) {
+            $("#status").text("Successfully logged in.");
+            window.location.replace('home');
+		} else {
+			$("#status").text("Error logging in.");
+		}
+	});
+}
+
+function logout() {
+	$.post("/logout", function(result) {
+		if (result && result.success) {
+			$("#status").text("Successfully logged out.");
+		} else {
+			$("#status").text("Error logging out.");
+		}
+	});
+}
